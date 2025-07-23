ef5db016650e254bd86def798f8ee9e9c0875afe
anantaditya10 <anant.aditya10@gmail.com>
2025-07-23 16:56:30 +0530

    debounce custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import { useEffect, useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import useDebounce from './useDebounce';
 
 function App() {
   const list = ['apple', 'banana', 'mango'];
   const [listItems, setListItems] = useState(list);
   const [value, setValue] = useState('');
+  const debounceValue=useDebounce(value, 200)
+  
   const handleChange = (e) => {
-    setValue(e.target.value);
-
+    setValue(e.target.value)
+    
     const filterList = list.filter((item) => {
-      return item.includes(e.target.value);
+      return item.includes(debounceValue);
     });
     setListItems(filterList);
   };
